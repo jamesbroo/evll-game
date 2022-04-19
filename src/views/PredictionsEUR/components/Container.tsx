4019e00c8c47fa2fa83b39dcf5141ddf881da640
raxioma <raxioma@outlook.com>
2022-04-19 21:38:57 +1000

    bg img

--- src/views/PredictionsEUR/components/Container.tsx
+++ src/views/PredictionsEUR/components/Container.tsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components'
 
 // background: ${({ theme }) => theme.colors.gradients.violetAlt};
+// background: #111520;
+
 const Container = styled.div`
-  background: #111520;
+  background-image: url("bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
   height: calc(100vh - 100px);
   overflow: hidden;
   position: relative;
